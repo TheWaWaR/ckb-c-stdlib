e7db8810c62276379f4f9c39d1a96ada61c1966a
Linfeng Qian <thewawar@gmail.com>
2020-12-02 18:30:50 +0800

    fix: fix comipile errors for c++
    Generate blockchain.h use moleculec v0.6.1

--- molecule/molecule_builder.h
+++ molecule/molecule_builder.h
@@ -2,9 +2,7 @@
 #define MOLECULE_BUILDER_H
 
 #ifdef __cplusplus
-#define _CPP_BEGIN extern "C" {
-#define _CPP_END }
-_CPP_BEGIN
+extern "C" {
 #endif /* __cplusplus */
 
 #include <stddef.h>
@@ -13,12 +11,22 @@ _CPP_BEGIN
 
 #include "molecule_reader.h"
 
+#ifndef MOLECULE_API_DECORATOR
+#define __DEFINE_MOLECULE_API_DECORATOR
+#define MOLECULE_API_DECORATOR
+#endif /* MOLECULE_API_DECORATOR */
+
 /*
  * This part is not for normal users.
  */
 
 // Test if the host is big endian machine.
-#define is_le() (*(unsigned char *)&(uint16_t){1})
+#define is_le()       \
+  ((union {           \
+     uint16_t i;      \
+     unsigned char c; \
+   }){.i = 1}         \
+       .c)
 
 /*
  * Definitions of types and simple utilities.
@@ -44,7 +52,7 @@ typedef struct {
 
 /* Utilities. */
 
-void mol_pack_number(uint8_t *dst, mol_num_t *num) {
+MOLECULE_API_DECORATOR void mol_pack_number(uint8_t *dst, mol_num_t *num) {
   const uint8_t *src = (const uint8_t *)num;
   if (is_le()) {
     memcpy(dst, src, MOL_NUM_T_SIZE);
@@ -60,13 +68,13 @@ void mol_pack_number(uint8_t *dst, mol_num_t *num) {
  * Core functions.
  */
 
-void mol_builder_discard(mol_builder_t builder) {
+MOLECULE_API_DECORATOR void mol_builder_discard(mol_builder_t builder) {
   free(builder.data_ptr);
   free(builder.number_ptr);
 }
 
-void mol_builder_initialize_fixed_size(mol_builder_t *builder,
-                                       mol_num_t fixed_size) {
+MOLECULE_API_DECORATOR void mol_builder_initialize_fixed_size(
+    mol_builder_t *builder, mol_num_t fixed_size) {
   if (fixed_size == 0) {
     builder->data_ptr = NULL;
     builder->data_used = 0;
@@ -82,10 +90,9 @@ void mol_builder_initialize_fixed_size(mol_builder_t *builder,
   builder->number_cap = 0;
 }
 
-void mol_union_builder_initialize(mol_builder_t *builder,
-                                  mol_num_t data_capacity, mol_num_t item_id,
-                                  const uint8_t *default_ptr,
-                                  mol_num_t default_len) {
+MOLECULE_API_DECORATOR void mol_union_builder_initialize(
+    mol_builder_t *builder, mol_num_t data_capacity, mol_num_t item_id,
+    const uint8_t *default_ptr, mol_num_t default_len) {
   builder->data_ptr = (uint8_t *)malloc(data_capacity);
   builder->data_cap = data_capacity;
   mol_pack_number(builder->data_ptr, &item_id);
@@ -100,9 +107,9 @@ void mol_union_builder_initialize(mol_builder_t *builder,
   builder->number_cap = 0;
 }
 
-void mol_builder_initialize_with_capacity(mol_builder_t *builder,
-                                          mol_num_t data_capacity,
-                                          mol_num_t number_capacity) {
+MOLECULE_API_DECORATOR void mol_builder_initialize_with_capacity(
+    mol_builder_t *builder, mol_num_t data_capacity,
+    mol_num_t number_capacity) {
   builder->data_ptr = (uint8_t *)malloc(data_capacity);
   builder->data_used = 0;
   builder->data_cap = data_capacity;
@@ -111,24 +118,24 @@ void mol_builder_initialize_with_capacity(mol_builder_t *builder,
   builder->number_cap = number_capacity;
 }
 
-void mol_fixvec_builder_initialize(mol_builder_t *builder,
-                                   mol_num_t data_capacity) {
+MOLECULE_API_DECORATOR void mol_fixvec_builder_initialize(
+    mol_builder_t *builder, mol_num_t data_capacity) {
   mol_builder_initialize_with_capacity(builder, data_capacity, MOL_NUM_T_SIZE);
   builder->number_ptr[0] = 0;
   builder->number_used = MOL_NUM_T_SIZE;
 }
 
-void mol_table_builder_initialize(mol_builder_t *builder,
-                                  mol_num_t data_capacity,
-                                  mol_num_t field_count) {
+MOLECULE_API_DECORATOR void mol_table_builder_initialize(
+    mol_builder_t *builder, mol_num_t data_capacity, mol_num_t field_count) {
   mol_builder_initialize_with_capacity(builder, data_capacity,
                                        MOL_NUM_T_SIZE * field_count * 2);
   memset(builder->number_ptr, 0x00, builder->number_cap);
   builder->number_used = builder->number_cap;
 }
 
-void mol_option_builder_set(mol_builder_t *builder, const uint8_t *data_ptr,
-                            mol_num_t data_len) {
+MOLECULE_API_DECORATOR void mol_option_builder_set(mol_builder_t *builder,
+                                                   const uint8_t *data_ptr,
+                                                   mol_num_t data_len) {
   builder->data_used = data_len;
   if (builder->data_used == 0) {
     builder->data_cap = 0;
@@ -144,8 +151,9 @@ void mol_option_builder_set(mol_builder_t *builder, const uint8_t *data_ptr,
   }
 }
 
-void mol_union_builder_set_byte(mol_builder_t *builder, mol_num_t item_id,
-                                uint8_t data) {
+MOLECULE_API_DECORATOR void mol_union_builder_set_byte(mol_builder_t *builder,
+                                                       mol_num_t item_id,
+                                                       uint8_t data) {
   builder->data_used = MOL_NUM_T_SIZE + 1;
   if (builder->data_cap < builder->data_used) {
     builder->data_cap = builder->data_used;
@@ -156,8 +164,10 @@ void mol_union_builder_set_byte(mol_builder_t *builder, mol_num_t item_id,
   *(builder->data_ptr + MOL_NUM_T_SIZE) = data;
 }
 
-void mol_union_builder_set(mol_builder_t *builder, mol_num_t item_id,
-                           const uint8_t *data_ptr, mol_num_t data_len) {
+MOLECULE_API_DECORATOR void mol_union_builder_set(mol_builder_t *builder,
+                                                  mol_num_t item_id,
+                                                  const uint8_t *data_ptr,
+                                                  mol_num_t data_len) {
   builder->data_used = MOL_NUM_T_SIZE + data_len;
   if (builder->data_cap < builder->data_used) {
     builder->data_cap = builder->data_used;
@@ -168,17 +178,20 @@ void mol_union_builder_set(mol_builder_t *builder, mol_num_t item_id,
   memcpy(builder->data_ptr + MOL_NUM_T_SIZE, data_ptr, data_len);
 }
 
-void mol_builder_set_byte_by_offset(mol_builder_t *builder, mol_num_t offset,
-                                    uint8_t data) {
+MOLECULE_API_DECORATOR void mol_builder_set_byte_by_offset(
+    mol_builder_t *builder, mol_num_t offset, uint8_t data) {
   *(builder->data_ptr + offset) = data;
 }
 
-void mol_builder_set_by_offset(mol_builder_t *builder, mol_num_t offset,
-                               const uint8_t *data_ptr, mol_num_t length) {
+MOLECULE_API_DECORATOR void mol_builder_set_by_offset(mol_builder_t *builder,
+                                                      mol_num_t offset,
+                                                      const uint8_t *data_ptr,
+                                                      mol_num_t length) {
   memcpy(builder->data_ptr + offset, data_ptr, length);
 }
 
-void mol_fixvec_builder_push_byte(mol_builder_t *builder, uint8_t data) {
+MOLECULE_API_DECORATOR void mol_fixvec_builder_push_byte(mol_builder_t *builder,
+                                                         uint8_t data) {
   while (builder->data_cap < builder->data_used + 1) {
     builder->data_cap *= 2;
     builder->data_ptr =
@@ -189,8 +202,9 @@ void mol_fixvec_builder_push_byte(mol_builder_t *builder, uint8_t data) {
   builder->data_used += 1;
 }
 
-void mol_fixvec_builder_push(mol_builder_t *builder, const uint8_t *data_ptr,
-                             mol_num_t length) {
+MOLECULE_API_DECORATOR void mol_fixvec_builder_push(mol_builder_t *builder,
+                                                    const uint8_t *data_ptr,
+                                                    mol_num_t length) {
   while (builder->data_cap < builder->data_used + length) {
     builder->data_cap *= 2;
     builder->data_ptr =
@@ -201,8 +215,9 @@ void mol_fixvec_builder_push(mol_builder_t *builder, const uint8_t *data_ptr,
   builder->data_used += length;
 }
 
-void mol_dynvec_builder_push(mol_builder_t *builder, const uint8_t *data_ptr,
-                             mol_num_t data_len) {
+MOLECULE_API_DECORATOR void mol_dynvec_builder_push(mol_builder_t *builder,
+                                                    const uint8_t *data_ptr,
+                                                    mol_num_t data_len) {
   while (builder->data_cap < builder->data_used + data_len) {
     builder->data_cap *= 2;
     builder->data_ptr =
@@ -224,8 +239,9 @@ void mol_dynvec_builder_push(mol_builder_t *builder, const uint8_t *data_ptr,
   }
 }
 
-void mol_table_builder_add_byte(mol_builder_t *builder, mol_num_t field_index,
-                                uint8_t data) {
+MOLECULE_API_DECORATOR void mol_table_builder_add_byte(mol_builder_t *builder,
+                                                       mol_num_t field_index,
+                                                       uint8_t data) {
   while (builder->data_cap < builder->data_used + 1) {
     builder->data_cap *= 2;
     builder->data_ptr =
@@ -238,8 +254,10 @@ void mol_table_builder_add_byte(mol_builder_t *builder, mol_num_t field_index,
   builder->data_used += 1;
 }
 
-void mol_table_builder_add(mol_builder_t *builder, mol_num_t field_index,
-                           const uint8_t *data_ptr, mol_num_t data_len) {
+MOLECULE_API_DECORATOR void mol_table_builder_add(mol_builder_t *builder,
+                                                  mol_num_t field_index,
+                                                  const uint8_t *data_ptr,
+                                                  mol_num_t data_len) {
   if (data_len == 0) {
     builder->number_ptr[field_index * 2] = 0;
     builder->number_ptr[field_index * 2 + 1] = 0;
@@ -257,7 +275,8 @@ void mol_table_builder_add(mol_builder_t *builder, mol_num_t field_index,
   }
 }
 
-mol_seg_res_t mol_builder_finalize_simple(mol_builder_t builder) {
+MOLECULE_API_DECORATOR mol_seg_res_t
+mol_builder_finalize_simple(mol_builder_t builder) {
   mol_seg_res_t res;
   res.errno = MOL_OK;
   res.seg.ptr = builder.data_ptr;
@@ -266,7 +285,8 @@ mol_seg_res_t mol_builder_finalize_simple(mol_builder_t builder) {
   return res;
 }
 
-mol_seg_res_t mol_fixvec_builder_finalize(mol_builder_t builder) {
+MOLECULE_API_DECORATOR mol_seg_res_t
+mol_fixvec_builder_finalize(mol_builder_t builder) {
   mol_seg_res_t res;
   res.errno = MOL_OK;
   res.seg.size = MOL_NUM_T_SIZE + builder.data_used;
@@ -279,24 +299,23 @@ mol_seg_res_t mol_fixvec_builder_finalize(mol_builder_t builder) {
   return res;
 }
 
-mol_seg_res_t mol_dynvec_builder_finalize(mol_builder_t builder) {
+MOLECULE_API_DECORATOR mol_seg_res_t
+mol_dynvec_builder_finalize(mol_builder_t builder) {
   mol_seg_res_t res;
   res.errno = MOL_OK;
   res.seg.size = MOL_NUM_T_SIZE + builder.number_used + builder.data_used;
   res.seg.ptr = (uint8_t *)malloc(res.seg.size);
   mol_pack_number(res.seg.ptr, &res.seg.size);
-  if (builder.data_used > 0) {
-    mol_num_t number_count = builder.number_used / MOL_NUM_T_SIZE;
-    mol_num_t header_size = MOL_NUM_T_SIZE + builder.number_used;
-    for (mol_num_t number_index = 0; number_index < number_count;
-         number_index++) {
-      builder.number_ptr[number_index] += header_size;
-    }
-    memcpy((res.seg.ptr + MOL_NUM_T_SIZE), builder.number_ptr,
-           builder.number_used);
-    memcpy((res.seg.ptr + MOL_NUM_T_SIZE + builder.number_used),
-           builder.data_ptr, builder.data_used);
+  mol_num_t number_count = builder.number_used / MOL_NUM_T_SIZE;
+  mol_num_t header_size = MOL_NUM_T_SIZE + builder.number_used;
+  for (mol_num_t number_index = 0; number_index < number_count;
+       number_index++) {
+    builder.number_ptr[number_index] += header_size;
   }
+  memcpy((res.seg.ptr + MOL_NUM_T_SIZE), builder.number_ptr,
+         builder.number_used);
+  memcpy((res.seg.ptr + MOL_NUM_T_SIZE + builder.number_used), builder.data_ptr,
+         builder.data_used);
   mol_builder_discard(builder);
   return res;
 }
@@ -307,10 +326,13 @@ mol_seg_res_t mol_dynvec_builder_finalize(mol_builder_t builder) {
 
 #undef is_le
 
+#ifdef __DEFINE_MOLECULE_API_DECORATOR
+#undef MOLECULE_API_DECORATOR
+#undef __DEFINE_MOLECULE_API_DECORATOR
+#endif /* __DEFINE_MOLECULE_API_DECORATOR */
+
 #ifdef __cplusplus
-_CPP_END
-#undef _CPP_BEGIN
-#undef _CPP_END
+}
 #endif /* __cplusplus */
 
 #endif /* MOLECULE_BUILDER_H */
